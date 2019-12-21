811dd8a3fdd13b353f781c16b1b5612674a3edb1
Rueangyot Treemat <ruengyod1@gmail.com>
2019-12-21 23:54:21 +0700

    Upload and Retrieve Image with Firebase in Android Studio

--- app/src/main/java/com/example/firestoreexample/MainActivity.java
+++ app/src/main/java/com/example/firestoreexample/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.firestoreexample;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +37,9 @@ public class MainActivity extends AppCompatActivity {
     final int IMAGE_REQUEST = 71;
     Uri imageLocationPath;
 
+    StorageReference objectStorageReference;
+    FirebaseFirestore objectFirebaseFirestore;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         uploadPicET = findViewById(R.id.imageNameET);
         uploadPicIV = findViewById(R.id.imageID);
 
+        objectStorageReference = FirebaseStorage.getInstance().getReference("imageFolder"); // Create folder to Firebase Storage
+        objectFirebaseFirestore = FirebaseFirestore.getInstance();
     }
 
     public void selectImage(View view){
@@ -58,6 +79,81 @@ public class MainActivity extends AppCompatActivity {
                 uploadPicIV.setImageBitmap(objectBitmap);
             }
 
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void uploadImage(View view){
+        try {
+            if(!uploadPicET.getText().toString().isEmpty() && imageLocationPath != null){
+                String nameOfimage = uploadPicET.getText().toString() + "." + getExtension(imageLocationPath);
+
+                final StorageReference imageRef = objectStorageReference.child(nameOfimage);
+
+                UploadTask objectUploadTask = imageRef.putFile(imageLocationPath);
+
+                objectUploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                    @Override
+                    public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                        if(!task.isSuccessful()){
+                           throw task.getException();
+                        }
+                        return imageRef.getDownloadUrl();
+                    }
+                }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        if (task.isSuccessful()){
+                            Map<String,String> objectMap = new HashMap<>();
+                            objectMap.put("uri", task.getResult().toString());
+
+                            objectFirebaseFirestore.collection("Links").document(uploadPicET.getText().toString())
+                                    .set(objectMap)
+                                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(MainActivity.this, "Image is uploaded",Toast.LENGTH_SHORT).show();
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(MainActivity.this, "Fails to upload images",Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+
+                        } else if (!task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            } else {
+                Toast.makeText(this, "Please provide name for image", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private String getExtension(Uri uri){
+        try {
+            ContentResolver objectContentResolver = getContentResolver();
+            MimeTypeMap objectMimeTypeMap = MimeTypeMap.getSingleton();
+
+            return objectMimeTypeMap.getExtensionFromMimeType(objectContentResolver.getType(uri));
+
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+        return null;
+    }
+
+    public void moveToShowImageAct(View view){
+        try {
+            startActivity(new Intent(this, ShowImageActivity.class));
         } catch (Exception e){
             Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
         }
