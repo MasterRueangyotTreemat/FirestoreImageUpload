807287206797898fa23b79466a8974e0baa81cfa
Rueangyot Treemat <ruengyod1@gmail.com>
2019-12-21 17:54:33 +0700

    Upload and Retrieve Image with Firebase in Android Studio

--- app/src/main/java/com/example/firestoreexample/MainActivity.java
+++ app/src/main/java/com/example/firestoreexample/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.firestoreexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView uploadPicIV;
+    EditText uploadPicET;
+
+    final int IMAGE_REQUEST = 71;
+    Uri imageLocationPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uploadPicET = findViewById(R.id.imageNameET);
+        uploadPicIV = findViewById(R.id.imageID);
+
+    }
+
+    public void selectImage(View view){
+        try {
+            Intent objectIntent = new Intent();
+            objectIntent.setType("image/*");
+
+            objectIntent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(objectIntent,IMAGE_REQUEST);
+
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        try {
+            if (requestCode == IMAGE_REQUEST
+                    && resultCode == RESULT_OK
+                    && data != null
+                    && data.getData() != null){
+                imageLocationPath = data.getData();
+                Bitmap objectBitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageLocationPath);
+                uploadPicIV.setImageBitmap(objectBitmap);
+            }
+
+        } catch (Exception e){
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
+
--- app/src/main/java/com/example/firestoreexample/ShowImageActivity.java
+++ app/src/main/java/com/example/firestoreexample/ShowImageActivity.java
@@ -1,8 +1,17 @@
 package com.example.firestoreexample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class ShowImageActivity extends AppCompatActivity {
 
@@ -10,5 +19,7 @@ public class ShowImageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_image);
+
     }
+
 }
